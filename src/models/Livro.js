28f102d15e8effc1362a6fcffc1b437d624404bc
Elias de Souza <elias99-souza@hotmail.com>
2023-07-16 17:21:48 -0300

    modelsLivros

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -5,8 +5,20 @@ const livroSchema = new mongoose.Schema(
     id: { type: String },
     titulo: { type: String, required: [true, ' titulo é obrigatório'] },
     autor: { type: String, required: [true, ' autor é obrigatório'] },
-    editora: { type: String, required: [true, ' editora é obrigatório'] },
-    numeroPaginas: { type: Number }
+    editora: {
+      type: String,
+      required: [true, ' editora é obrigatório'],
+      enum: {
+        values: ['Casa do código', 'Alura'],
+        message: 'A editora {VALUE} não é um valor permitido'
+      }
+    },
+    numeroPaginas: {
+      type: Number,
+      min: [10, 'O número de páginas deve estar entre 10 e 5000 páginas. Valor fornecido: {VALUE}'],
+      max: [5000, 'O número de páginas deve estar entre 10 e 5000 páginas.Valor fornecido: {VALUE}']
+
+    }
   }
 );
 
